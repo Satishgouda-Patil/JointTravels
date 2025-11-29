0412bbae50cb0e68242235863b31d844b511ced4
satish-patil <satishgoudapatil85@gamil.com>
2025-11-29 22:35:17 +0530

    added url of momgo

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Connected to MongoDB");
   });
-await mongoose.connect(process.env.MONGODB_URI);
+await mongoose.connect(process.env.MONGO_URI);
 };
 
 export default connectDB;
